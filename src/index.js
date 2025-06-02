4a0b724dd60287e9f2c12fb2792e3b7641ce09a8
Franclin Gonzales--FactorX <franclin.gonzalez@tecsup.edu.pe>
2025-06-02 16:59:53 -0500

    dswsd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { Pool } = require('pg');
 const axios = require('axios');
 const productsRouter = require('./routes/products');
+const cors = require('cors');
+
 
 // Configuración de la base de datos PostgreSQL
 const pool = new Pool({
@@ -25,3 +27,12 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 }); 
+
+
+
+// Configura CORS para permitir solicitudes desde localhost y tu dominio
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://examen3awa.onrender.com'],  // Ajusta según sea necesario
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
